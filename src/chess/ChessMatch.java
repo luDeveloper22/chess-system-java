959613b2e16495ac157e0c1b14a9bbf98a823a9d
unknown <fee_lm.c@hotmail.com>
2020-08-12 20:08:48 -0300

    Little improvement in board printing

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -32,9 +32,20 @@ public class ChessMatch {
 	}
 	
 	private void initialSetup() {
-		placeNewPiece('b', 6, new Rock(board, Color.white));
-		placeNewPiece('e', 8, new King(board, Color.BLACK));
-		placeNewPiece('e', 1, new King(board, Color.white));
-	}
+		placeNewPiece('c', 1, new Rock(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rock(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rock(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rock(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rock(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
 
+        placeNewPiece('c', 7, new Rock(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rock(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rock(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rock(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rock(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
+
+}
+	
 }
--- src/chess/Color.java
+++ src/chess/Color.java
@@ -2,5 +2,5 @@ package chess;
 
 public enum Color {
     BLACK,
-    white;
+    WHITE;
 }
