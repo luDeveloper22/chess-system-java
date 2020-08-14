e254d36b4c9b8e937528c8698a5eff435edf8209
unknown <fee_lm.c@hotmail.com>
2020-08-13 21:37:41 -0300

    Possible move of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,4 +1,5 @@
 package chess;
+import chess.ChessException;
 import chess.pieces.Rock;
 
 import chess.pieces.King;
@@ -18,17 +19,14 @@ public class ChessMatch {
 	}
 
 	public ChessPiece[][] getPieces() {
-		 ChessPiece[][] mat = new ChessPiece[board.getRow()][board.getColumn()];
-		 
-		 for (int i = 0; i < board.getRow(); i++ ) {
-			for (int j = 0; j < board.getColumn(); j++ ) {
-				mat[i][j] = (ChessPiece)board.piece(i, j);
+		ChessPiece[][] mat = new ChessPiece[board.getRow()][board.getColumn()];
+		for (int i=0; i<board.getRow(); i++) {
+			for (int j=0; j<board.getColumn(); j++) {
+				mat[i][j] = (ChessPiece) board.piece(i, j);
 			}
-		 }
-		 
-		 return mat;
-	 }
-	
+		}
+		return mat;
+	}
 	
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition();
@@ -48,6 +46,9 @@ public class ChessMatch {
 	private void validateSourcePosition(Position position) {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source position");
+		}
+		if (!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("There is no possible moves for the chosen piece");
 		}
 	}
 	
