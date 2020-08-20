eb784f55b87d82183b59e8698f3c0e1100878fc3
unknown <fee_lm.c@hotmail.com>
2020-08-19 22:35:44 -0300

    switching player each turn

--- src/applicaton/UI.java
+++ src/applicaton/UI.java
@@ -4,6 +4,7 @@ import chess.Color;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
@@ -50,6 +51,14 @@ public class UI {
 				throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
 			}
 		}
+		
+		public static void printMatch(ChessMatch chessMatch) {
+			printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.println("Turn : " + chessMatch.getTurn());
+			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+		}
+		
 		
 		public static void printBoard(ChessPiece[][] pieces) {
 			for (int i = 0; i < pieces.length; i++) {
